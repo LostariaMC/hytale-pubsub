08458f2a2cf4a45c191ad8227fa8506fee15bffa
Erpriex <erpriex@gmail.com>
2026-02-11 22:00:07 +0100

    👽️ Switch to new imports Jackson JsonNode

--- src/main/java/fr/lostaria/hytalepubsub/payload/MessageEnvelope.java
+++ src/main/java/fr/lostaria/hytalepubsub/payload/MessageEnvelope.java
@@ -1,9 +1,9 @@
 package fr.lostaria.hytalepubsub.payload;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import tools.jackson.databind.JsonNode;
 
 @Getter
 @Setter
